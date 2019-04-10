a1462d3975b95c53aec9ce42e4e190f5b756eeee
harunrasyid98 <49441617+harunrasyid98@users.noreply.github.com>
2019-04-10 08:54:46 +0700

    Update function.h

--- function.h
+++ function.h
@@ -16,4 +16,11 @@ char* getAsisten(char as[2]);
 char* getMatkul(int index);
 char* getHari(int index);
 
+int getIndexAsisten (char str);
+int getIndexMinggu (char *str);
+int getIndexLab (char *str);    
+int getIndexHari (char *str);
+void AssignAsisten(char listAsisten[12][5][4][2], praktikum listPraktikum[12][5][4]);
+
+
 #endif // FUNCTION_H
